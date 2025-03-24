a9cde88872e1e782d22b4b9f4bba0d4b815a73e4
Miskachev Dima <miskachevds@yandex.ru>
2025-03-24 16:51:50 +0300

    api hh finish

--- src/pages/main/Vacancies.js
+++ src/pages/main/Vacancies.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import { getAllVacancies } from "../../api";
+
 function Vacancies(){
+    const[catalog, setCatalog] = useState([]);
+
+    useEffect(() => {
+        getAllVacancies().then(data => {
+            setCatalog(data.vacancies)
+        })
+    },[]);
+
+    console.log(catalog)
+
     return(
         <div className="wrap">
             <h2>Здесь будет вся Api вакансий</h2>
