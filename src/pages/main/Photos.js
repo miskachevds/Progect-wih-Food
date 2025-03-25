eb49ee39ed701c06982f1d30a452edd2142b00ec
Miskachev Dima <miskachevds@yandex.ru>
2025-03-25 15:36:53 +0300

    start api photo

--- src/pages/main/Photos.js
+++ src/pages/main/Photos.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { getAllPhotos } from "../../api";
+
 function Photos(){
-    
+    const [catalog, setCatalog] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(data => setCatalog(data))
+        // console.log(catalog);
+    })
+    // const [catalog, setCatalog] = useState([])
+
+    // useEffect(() => {
+    //     getAllPhotos().then(data => {
+    //         setCatalog(data)
+    //     })
+    // },[]);
+
+    // console.log(catalog);
+
     return(
         <div className="wrap">
             <h2>Здесь будет вся Api фото</h2>
