fbc1fb70b67e3ab0969438b6d48af8fa5b217309
Miskachev Dima <miskachevds@yandex.ru>
2025-04-03 16:44:08 +0300

    video+

--- src/components/FotoList.js
+++ src/components/FotoList.js
@@ -1,25 +1,29 @@
 import Foto from "./Foto";
 
-function FotoList({allCat}){//принимаем пропс 
-    return(
+function FotoList({ allCat }) {
+    return (
         <div className="list">
             {
-                allCat.map(item => (//метод map для перебора массива allcat,для каждого эл item в массиве создается Foto
-                    <Foto key={item.idCategory} {...item} />//ключ key={item.idCategory} это уник ключ для каждого эл списка кот помогает react оптимизировать процесс рендеринга
-                )) //...item это синтк распространения кот,передает все свойства объекта item как отдельные пропсы в компонент Foto
+                allCat.map(item => (
+                    <Foto key={item.idCategory} {...item} />
+                ))
             }
-            
             {
-                allCat.strYoutube ? (<div>
-                    <h4>Video flet</h4>
-                    <div className="video-wrap">
-                    <iframe src="" title="YouTube video player" frameborder="0" ></iframe>
-                    </div>
-                    </div>) : null
+                allCat.map(item => (
+                    item.strYoutube ? (//если видео есть то показываем
+                        <div key={item.idCategory}>
+                            <h4>Видео презентация</h4>
+                            <div className="video-wrap">
+                                <iframe title="Video" src={`https://www.youtube.com/
+                                embed/${item.strYoutube.slice(-11)}`}
+                                frameBorder="0" allowFullScreen></iframe>
+                            </div>
+                        </div>
+                    ) : null
+                ))
             }
         </div>
-        
-    )
+    );
 }
 
 export default FotoList;
